f203127bfd5e52329121d27490b72faed7085ce0
Hyein Kim <hyeinisfree.dev@gmail.com>
2025-04-30 22:05:48 +0900

    ➕ Openfeign 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,12 +17,22 @@ repositories {
 	mavenCentral()
 }
 
+val springCloudVersion = "2024.0.0"
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
+	}
+}
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-aop")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
+
+	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 
 	implementation("org.flywaydb:flyway-mysql")
 
