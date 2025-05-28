26bb1213de1b621f415bd33420489cf06309f8f9
Hyein Kim <hyeinisfree.dev@gmail.com>
2025-05-29 00:51:11 +0900

    🩹 FoodRepository 파일 내 불필요한 공백 제거

--- src/main/java/com/example/matdongsan/repository/FoodRepository.java
+++ src/main/java/com/example/matdongsan/repository/FoodRepository.java
@@ -3,6 +3,6 @@ package com.example.matdongsan.repository;
 import com.example.matdongsan.domain.Food;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FoodRepository  extends JpaRepository<Food, Long> {
+public interface FoodRepository extends JpaRepository<Food, Long> {
     boolean existsByName(String name);
 }
