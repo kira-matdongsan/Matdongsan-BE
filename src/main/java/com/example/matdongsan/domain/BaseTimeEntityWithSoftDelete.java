e25c9c9a7ab25214bae053cfd08146bdb0051eda
Hyein Kim <hyeinisfree.dev@gmail.com>
2025-05-29 00:18:53 +0900

    ✨ BaseTimeEntityWithSoftDelete 클래스 추가

---
+++ src/main/java/com/example/matdongsan/domain/BaseTimeEntityWithSoftDelete.java
@@ -0,0 +1,24 @@
+package com.example.matdongsan.domain;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntityWithSoftDelete extends BaseTimeEntity {
+
+    private LocalDateTime deletedAt;
+
+    public void softDelete() {
+        this.deletedAt = LocalDateTime.now();
+    }
+
+    public boolean isDeleted() {
+        return this.deletedAt != null;
+    }
+}
