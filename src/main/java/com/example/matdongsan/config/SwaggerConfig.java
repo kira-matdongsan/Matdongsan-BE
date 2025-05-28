bdf9f21e792870a320151e9341c8b26ed13e1f06
Hyein Kim <hyeinisfree.dev@gmail.com>
2025-05-29 00:17:17 +0900

    🔧 Swagger 그룹 및 태그 순서 정렬을 위한 설정 변경

--- src/main/java/com/example/matdongsan/config/SwaggerConfig.java
+++ src/main/java/com/example/matdongsan/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.example.matdongsan.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,11 +12,44 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         Info info = new Info()
-                .title("Demo Application API Document")
-                .version("v0.0.1")
-                .description("Demo Application API 명세서입니다.");
+                .title("Matdongsan Application API Document")
+                .version("v1")
+                .description("Matdongsan Application API 명세서입니다.");
 
         return new OpenAPI()
                 .info(info);
+    }
+
+    @Bean
+    public GroupedOpenApi userApi() {
+        return GroupedOpenApi.builder()
+                .group("1-User API")
+                .pathsToMatch("/api/v1/**")
+                .pathsToExclude("/api/v1/external/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi docsApi() {
+        return GroupedOpenApi.builder()
+                .group("2-Docs API")
+                .pathsToMatch("/docs/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi externalApi() {
+        return GroupedOpenApi.builder()
+                .group("3-External API")
+                .pathsToMatch("/api/v1/external/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi sampleApi() {
+        return GroupedOpenApi.builder()
+                .group("4-Sample API")
+                .pathsToMatch("/sample/**")
+                .build();
     }
 }
