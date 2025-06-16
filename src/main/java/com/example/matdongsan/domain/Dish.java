485ccde2baa91e249c526315a468279e427499d0
Hyein Kim <hyeinisfree.dev@gmail.com>
2025-06-16 22:19:59 +0900

    🗃️ Dish Entity userId 삭제, 편의 메서드 추가

--- src/main/java/com/example/matdongsan/domain/Dish.java
+++ src/main/java/com/example/matdongsan/domain/Dish.java
@@ -1,8 +1,14 @@
 package com.example.matdongsan.domain;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SuperBuilder
 @Getter
@@ -16,12 +22,21 @@ public class Dish extends BaseTimeEntityWithSoftDelete {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "food_id", nullable = false)
+    private Food food;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "featured_food_id", nullable = false)
     private FeaturedFood featuredFood;
-
-    private Long userId;
 
     private String name;
 
     private Integer voteCount = 0;
+
+    @OneToMany(mappedBy = "dish", cascade = CascadeType.ALL)
+    private List<DishVoteImage> images = new ArrayList<>();
+
+    public void plusVoteCount() {
+        voteCount++;
+    }
 }
