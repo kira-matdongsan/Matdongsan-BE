305424be5c5c8d2036760ea5ad84ed9f68ea96ca
Hyein Kim <hyeinisfree.dev@gmail.com>
2025-06-21 17:38:38 +0900

    📝 [Auth] 인증 관련 API Swagger 코드 작성

--- src/main/java/com/example/matdongsan/controller/AuthController.java
+++ src/main/java/com/example/matdongsan/controller/AuthController.java
@@ -7,46 +7,69 @@ import com.example.matdongsan.common.response.ResultResponse;
 import com.example.matdongsan.controller.dto.SignupRequestDto;
 import com.example.matdongsan.controller.dto.TermsResponseDto;
 import com.example.matdongsan.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Tag(name = "인증 API", description = "인증 관련 API")
+@Validated
 @RequiredArgsConstructor
+@RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
     private final AuthService authService;
 
     // 약관 목록 조회
+    @Operation(summary = "약관 목록 조회", description = "전체 약관 목록 조회 (상세 내용 포함)")
     @GetMapping("/terms")
     public ResponseEntity<CommonResponse<ListResponse<TermsResponseDto>>> getAllTerms() {
         return CommonResponse.successList(ResponseCode.OK, authService.getAllTerms());
     }
 
     // 인증 번호 메일 발송
+    @Operation(summary = "인증 번호 메일 발송", description = "요청된 메일로 인증 번호 발송")
     @PostMapping("/email-verification/request")
-    public ResponseEntity<CommonResponse<ResultResponse<Boolean>>> requestVerification(@RequestParam String email) {
+    public ResponseEntity<CommonResponse<ResultResponse<Boolean>>> requestVerification(
+            @Parameter(name = "email", description = "인증 번호를 발송할 이메일 주소", example = "test@test.com")
+            @RequestParam String email
+    ) {
         authService.sendVerificationEmail(email);
         return CommonResponse.successResult(ResponseCode.OK, true);
     }
 
     // 인증 번호 검증
+    @Operation(summary = "인증 번호 검증", description = "인증 번호를 입력하여 검증")
     @PostMapping("/email-verification/verify")
-    public ResponseEntity<CommonResponse<ResultResponse<Boolean>>> verifyCode(@RequestParam String email, @RequestParam String code) {
+    public ResponseEntity<CommonResponse<ResultResponse<Boolean>>> verifyCode(
+            @Parameter(name = "email", description = "인증 번호를 수신한 이메일 주소", example = "test@test.com")
+            @RequestParam String email,
+            @Parameter(name = "code", description = "메일로 수신한 인증 번호", example = "1234")
+            @RequestParam String code
+    ) {
         authService.verifyCode(email, code);
         return CommonResponse.successResult(ResponseCode.OK, true);
     }
 
     // 이메일 중복 검사
+    @Operation(summary = "이메일 중복 검사", description = "이미 가입된 이메일 주소인지 확인")
     @GetMapping("/check-email")
-    public ResponseEntity<CommonResponse<ResultResponse<Boolean>>> checkEmail(@RequestParam String email) {
+    public ResponseEntity<CommonResponse<ResultResponse<Boolean>>> checkEmail(
+            @Parameter(name = "email", description = "중복 검사할 이메일 주소")
+            @RequestParam String email
+    ) {
         return CommonResponse.successResult(ResponseCode.OK, authService.checkEmailAvailable(email));
     }
 
     // 이메일 회원가입
+    @Operation(summary = "이메일 회원가입")
     @PostMapping("/signup")
-    public ResponseEntity<CommonResponse<ResultResponse<Boolean>>> signup(@RequestBody SignupRequestDto requestDto) {
+    public ResponseEntity<CommonResponse<ResultResponse<Boolean>>> signup(@RequestBody @Valid SignupRequestDto requestDto) {
         authService.signup(requestDto.toServiceDto());
         return CommonResponse.successResult(ResponseCode.OK, true);
     }
--- src/main/java/com/example/matdongsan/controller/dto/SignupRequestDto.java
+++ src/main/java/com/example/matdongsan/controller/dto/SignupRequestDto.java
@@ -2,6 +2,7 @@ package com.example.matdongsan.controller.dto;
 
 import com.example.matdongsan.service.dto.SignupServiceDto;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -12,8 +13,15 @@ import java.util.List;
 @Getter
 public class SignupRequestDto {
 
+    @Schema(description = "이메일 주소", example = "test@test.com")
+    @NotBlank(message = "이메일을 입력해주세요.")
     private String email;
+
+    @Schema(description = "비밀번호", example = "password123!!")
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
+
+    @Schema(description = "동의하는 약관 ID 목록", example = "[1,2,3]")
     private List<Long> termsIds;
 
     public SignupServiceDto toServiceDto() {
