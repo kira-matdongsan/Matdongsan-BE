8a862fbf3f34a16a24898045cb24c1047b961e26
Hyein Kim <hyeinisfree.dev@gmail.com>
2025-07-22 14:54:59 +0900

    ✨ [Auth] Oauth2 카카오 로그인 닉네임, 프로필 이미지 Nullable 처리 추가

--- src/main/java/com/example/matdongsan/external/oauth/OauthService.java
+++ src/main/java/com/example/matdongsan/external/oauth/OauthService.java
@@ -3,15 +3,17 @@ package com.example.matdongsan.external.oauth;
 import com.example.matdongsan.domain.LoginType;
 import com.example.matdongsan.external.oauth.dto.OauthResponseDto;
 import com.example.matdongsan.external.oauth.kakao.KakaoOauthClient;
+import com.example.matdongsan.external.oauth.kakao.dto.KakaoAccount;
+import com.example.matdongsan.external.oauth.kakao.dto.KakaoProfile;
 import com.example.matdongsan.external.oauth.kakao.dto.KakaoUserResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Service
-@Slf4j
 public class OauthService {
 
     @Value("${external.kakao.oauth.client-id}")
@@ -33,11 +35,21 @@ public class OauthService {
         String authorization = "Bearer " + token;
         KakaoUserResponse response = kakaoOauthClient.getUserInfo(authorization);
 
+        String nickname = Optional.ofNullable(response.getKakaoAccount())
+                .map(KakaoAccount::getProfile)
+                .map(KakaoProfile::getNickname)
+                .orElse(null);
+
+        String profileImageUrl = Optional.ofNullable(response.getKakaoAccount())
+                .map(KakaoAccount::getProfile)
+                .map(KakaoProfile::getProfileImageUrl)
+                .orElse(null);
+
         return OauthResponseDto.builder()
                 .oauthId(response.getId().toString())
                 .email(response.getKakaoAccount().getEmail())
-                .nickname(response.getKakaoAccount().getProfile().getNickname())
-                .profileImageUrl(response.getKakaoAccount().getProfile().getProfileImageUrl())
+                .nickname(nickname)
+                .profileImageUrl(profileImageUrl)
                 .build();
     }
 
